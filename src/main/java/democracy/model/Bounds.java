e0cc78921c3a3d005eecdeef3b7a853f011bd2cc
jplanson <jplanson@wpi.edu>
2019-10-24 11:51:50 -0400

    Small addition to bounds model

--- src/main/java/democracy/model/Bounds.java
+++ src/main/java/democracy/model/Bounds.java
@@ -2,17 +2,24 @@ package democracy.model;
 
 public class Bounds 
 {
+	private final int id;
 	private final int x; 
 	private final int y;
 	private final int width;
 	private final int height;
 	
-	public Bounds(int x, int y, int width, int height)
+	public Bounds(int id, int x, int y, int width, int height)
 	{
+		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+	}
+	
+	public int getId()
+	{
+		return id;
 	}
 
 	public int getX() 
