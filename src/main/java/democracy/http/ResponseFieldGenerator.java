7c63801fa476a51a8df272c12d7e3b78ed4cd9e7
jplanson <jplanson@wpi.edu>
2019-10-27 11:33:31 -0400

    finished getChangeCardListGenerator and ListCardHandler

--- src/main/java/democracy/http/ResponseFieldGenerator.java
+++ src/main/java/democracy/http/ResponseFieldGenerator.java
@@ -1,9 +1,26 @@
 package democracy.http;
 
+import java.util.List;
+
+import democracy.dao.CardDAO;
+import democracy.dao.EventDAO;
+import democracy.dao.LayoutDAO;
+import democracy.model.Card;
+import democracy.model.Event;
+import democracy.model.Layout;
+
 public class ResponseFieldGenerator 
 {
-	public ChangeCardListResponse getChangeCardListResponse()
+	public static ChangeCardListResponse getChangeCardListResponse() throws Exception
 	{
-		return null;
+		List<Card> cards;
+		List<Event> events;
+		List<Layout> layouts;
+		
+		cards = new CardDAO().getAllCards();
+		events = new EventDAO().getAllEvents();
+		layouts = new LayoutDAO().getAllLayouts();
+		
+		return new ChangeCardListResponse(cards.toArray(new Card[0]), events.toArray(new Event[0]), layouts.toArray(new Layout[0]));
 	}
 }
