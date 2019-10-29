578e7429d1aa6b5e412f129ecc9fdd808d02f3f0
Bowen <btang@wpi.edu>
2019-10-29 10:27:20 -0400

    finish lambda function for create card

--- src/main/java/democracy/lambda/CreateCardHandler.java
+++ src/main/java/democracy/lambda/CreateCardHandler.java
@@ -11,15 +11,31 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.Gson;
 
+import democracy.dao.CardDAO;
+import democracy.http.ChangeCardListResponse;
 import democracy.http.CreateCardRequest;
 import democracy.http.PostRequest;
 import democracy.http.PostResponse;
 import democracy.http.RequestResponse;
+import democracy.http.ResponseFieldGenerator;
+import democracy.model.Card;
 
 public class CreateCardHandler implements RequestStreamHandler 
 {
 	
 	LambdaLogger logger;
+	
+	boolean createCard(String recipient, int eventid) throws Exception {
+		if (logger != null) { logger.log("in createCard"); }
+		CardDAO dao = new CardDAO();
+		
+		// check if legal
+		if (recipient.length() > 0 && eventid >= 1 && eventid <= 17) {
+		Card card = new Card(1, eventid, recipient, 1);
+		return dao.addCard(card);
+		}
+		return false;
+	}
 	
 	@Override
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException 
@@ -36,6 +52,8 @@ public class CreateCardHandler implements RequestStreamHandler
 		responseJson.put("headers", headerJson);
 		
 		RequestResponse response = null;
+		
+		ChangeCardListResponse result;
 		
 		// extract body from incoming HTTP POST request. If any error, then return 422 error
 		String body;
@@ -68,8 +86,18 @@ public class CreateCardHandler implements RequestStreamHandler
 			CreateCardRequest req = new Gson().fromJson(body, CreateCardRequest.class);
 			logger.log(req.toString());
 			
-			// LOGIC OF LAMBDA FUNCTION		
-			
+			// LOGIC OF LAMBDA FUNCTION	
+			try {
+				if(createCard(req.recipientName, req.eventId)) {
+					result = ResponseFieldGenerator.getChangeCardListResponse();
+					response = new RequestResponse(200, result);
+				}
+				else {
+					response = new RequestResponse(422, "Unable to create card");	
+				}
+			}catch(Exception e) {
+				response = new RequestResponse(400, "Unable to create card for" + req.recipientName + "(" + e.getMessage() + ")");
+			}
 		}
 		
 		//last thing we do 
