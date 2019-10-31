12d43a43d606e63315259e807eaf3cae8569721e
jplanson <jplanson@wpi.edu>
2019-10-31 15:10:14 -0400

    added basic stuff for teammates to work on button press

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,8 @@
 var baseUrl = "https://yvmlvrpr1m.execute-api.us-east-1.amazonaws.com/alpha/"; 
 
 var listCardsUrl	= baseUrl + "main/list/cards";   // GET
+var deleteCardUrl	= baseUrl + "main/delete";
+var createCardUrl 	= baseUrl + "main/create";
 
 function refreshCardList()
 {
@@ -13,7 +15,14 @@ function refreshCardList()
 		if (xhr.readyState == XMLHttpRequest.DONE)
 		{
 		    var requestResponse = parseRequestResponse(xhr.responseText);
-			updateCardList(xhr.responseText);
+		    if (requestResponse[2] != null)
+		    {
+		    	updateCardList(requestResponse[2])
+		    }
+		    else
+		    {
+		    	console.log(requestResponse[1]);
+		    }
 		}
 		else
 		{
@@ -24,32 +33,56 @@ function refreshCardList()
 
 function parseRequestResponse(xhrResponseText)
 {
-	var xhrResponse = JSON.parse(jsonResponse);
+	var xhrResponse = JSON.parse(xhrResponseText);
 	var body = JSON.parse(xhrResponse["body"]);
 	
-	var statusCode = body["httpCode"];
-	var errorString = "";
-	var response = Null;
+	var statusCode = body["statusCode"];
+	var errorMessage = "";
+	var response = null;
 	
 	if (statusCode == 200)
 	{
 		response = body["response"];
+		console.log(response);
 	} 
 	else
 	{
-		errorString = body["errorString"];
+		errorMessage = body["errorMessage"];
 	}
+	
+	return [statusCode, errorMessage, response];
 }
 
-function updateCardList(jsonResponse)
-{
-	var response = JSON.parse(jsonResponse);
-	console.log(response["body"])
-	var body = JSON.parse(response["body"]);
-	
+function updateCardList(lambdaResponse)
+{	
 	var cardList = document.getElementById("cardList");
+	
+	var responseCardList = lambdaResponse["cards"]
 	
 	var output = "";
 	
+	for (var i = 0; i < responseCardList.length; i++)
+	{
+		var card = responseCardList[i];
+		var cardId = card.id;
+		var eventId = card.eventId;
+		var recipient = card.recipientName;
+		var layoutId = card.layoutId;
+		console.log(card);
+		
+		output += "<div id=\"card" + cardId + "\"><b> CARD: " + cardId + 
+			", " + recipient + ": " + eventId + "</b><br></div>";
+	}
+	
 	cardList.innerHTML = output;
+}
+
+function handleCreateCardClick()
+{
+
+}
+
+function handleDeleteCardClick()
+{
+	
 }
