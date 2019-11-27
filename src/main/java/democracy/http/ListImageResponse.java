e55852d452ae65bb50831ab7dacc49c733177870
LocNguyen <ndxloc1995@gmail.com>
2019-11-27 13:04:56 -0500

    Final Fix for List Image

--- src/main/java/democracy/http/ListImageResponse.java
+++ src/main/java/democracy/http/ListImageResponse.java
@@ -4,7 +4,7 @@ public class ListImageResponse extends ResponseField {
 
 	public String[] imageS3URL;
 	
-	public ListImageResponse(String[] imageBase64URL) {
+	public ListImageResponse(String[] imageS3URL) {
 		this.imageS3URL = imageS3URL;
 	}
 }
